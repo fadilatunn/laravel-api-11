696c44706793bc6c0ebcb19ca810c85fa4641f50
Fadila <fadilatun.ni41@smk.belajar.id>
2026-08-04 07:42:41 +0700

    tambah method put & post

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 })->middleware('auth:sanctum'); */
 
 Route::get('/product', [ProductController::class,'index'])->name('product');
+Route::post('/product', [ProductController::class,'store'])->name('product.store');
+Route::put('/product/{product}', [ProductController::class,'update'])->name('product.update');
+Route::patch('/product/{product}', [ProductController::class,'update']);
